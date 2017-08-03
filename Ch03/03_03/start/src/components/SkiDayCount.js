82d305768c05e16a4af6161fd8d6e244f881415c
Guinslym <guinslym@gmail.com>
2017-08-03 05:55:57 -0400

    initial commit

---
+++ Ch03/03_03/start/src/components/SkiDayCount.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../stylesheets/ui.scss'
+
+export const SkiDayCount = React.createClass({
+	render() {
+		return (
+			<div className="ski-day-count">
+				<div className="total-days">
+					<span>{ this.props.total }</span>
+				</div>
+				<div className="powder-days">
+					<span>{ this.props.powder }</span>
+				</div>
+				<div className="backcountry-days">
+					<span>{ this.props.backcountry}</span>
+				</div>
+				<div>
+					<span>{ this.props.goal }</span>
+				</div>
+			</div>
+		)
+	}
+})
